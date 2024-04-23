c0ec1da39bbdf255d3341f9f910bd010c9db573c
devgirlpro <agalidari@gmail.com>
2024-04-23 18:03:32 +0200

    add navber without all pages

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,12 +1,35 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Header = () => {
   return (
-    <div className='p-5 bg-primary flex justify-between' >
-         <h1 className='text-secondary text-xl font-semibold' >Azadeh Galidari</h1>
+    <>
+         {/* <h1 className='text-secondary text-xl font-semibold' >Azadeh Galidari</h1>
          <h1 className='text-secondary text-xl font-semibold' >Fullstack Web Developer</h1>
-         <h1 className='text-secondary text-xl font-semibold' >Portfolio</h1>
-    </div>
+         <h1 className='text-secondary text-xl font-semibold' >Portfolio</h1> */}
+         <nav className=' bg-primary'>
+            <ul className='p-5 bg-primary flex  justify-end ml-5' >
+                <li className='text-secondary text-xl font-semibold'>
+                    <Link to="/" >Home</Link>
+                </li>
+                <li className='text-secondary text-xl font-semibold ml-5'>
+                    <Link to="/about">About me</Link>
+                </li>
+                <li className='text-secondary text-xl font-semibold ml-5'>
+                    <Link to="/resume">Resume</Link>
+                </li>
+                <li className='text-secondary text-xl font-semibold ml-5'>
+                    <Link to="/projects">Projects</Link>
+                </li>
+                <li className='text-secondary text-xl font-semibold ml-5'>
+                    <Link to="/contact">Contact me</Link>
+                </li>
+                <li className='text-secondary text-xl font-semibold ml-5'>
+                    <Link to="/learn">Learn with me</Link>
+                </li>
+            </ul>
+         </nav>
+    </>
   )
 }
 
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import Header from '../components/Header';
+import Intro from './Intro';
 
 const Home = () => {
   return (
     <div>
-        <Header />
-      Home
-      
+      <Header />
+      <Intro />
     </div>
   );
 };
