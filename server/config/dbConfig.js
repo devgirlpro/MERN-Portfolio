25d6109884e7acd1c62e133863a7fa454876a1b8
devgirlpro <agalidari@gmail.com>
2024-04-25 20:07:40 +0200

    database and conectione setup done

---
+++ server/config/dbConfig.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+console.log("**********", process.env.mongo_url)
+mongoose.connect(process.env.mongo_url);
+
+const connection = mongoose.connection;
+
+connection.on('error', () => {
+  console.log('Error connecting to database');
+});
+
+connection.on('connected', () => {
+  console.log('mongoDB connected successfull');
+});
+
+module.exports = connection
