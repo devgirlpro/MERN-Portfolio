d32ab5460e68460f1ed4df94ea542ad3cf25c4fc
devgirlpro <agalidari@gmail.com>
2024-05-15 23:05:45 +0200

    ready to deploy

--- client/src/pages/LeftSider.jsx
+++ client/src/pages/LeftSider.jsx
@@ -1,30 +1,30 @@
-import React from 'react';
+// import React from 'react';
 
-const leftSider = () => {
-  return (
-    <div className="fixed left-0 bottom-0 px-5  sm:static">
-      <div className="flex flex-col items-center">
-        <div className="flex flex-col gap-3 text-[#915EFF] sm:flex-row">
-          <a
-            href="https://www.linkedin.com/in/azadeh-galidari/"
-            target="_blank"
-          >
-            <i className="ri-linkedin-fill "></i>
-          </a>
-          <a href="https://github.com/devgirlpro" target="_blank">
-            <i className="ri-github-fill "></i>
-          </a>
-          <a href="">
-            <i className="ri-mail-send-fill "></i>
-          </a>
-          <a href="">
-            <i className="ri-phone-fill "></i>
-          </a>
-        </div>
-        <div className="w-[1px] h-32 bg-[#915EFF] sm:hidden"></div>
-      </div>
-    </div>
-  );
-};
+// const leftSider = () => {
+//   return (
+//     <div className="fixed left-0 bottom-0 px-5  sm:static">
+//       <div className="flex flex-col items-center">
+//         <div className="flex flex-col gap-3 text-[#915EFF] sm:flex-row">
+//           <a
+//             href="https://www.linkedin.com/in/azadeh-galidari/"
+//             target="_blank"
+//           >
+//             <i className="ri-linkedin-fill "></i>
+//           </a>
+//           <a href="https://github.com/devgirlpro" target="_blank">
+//             <i className="ri-github-fill "></i>
+//           </a>
+//           <a href="">
+//             <i className="ri-mail-send-fill "></i>
+//           </a>
+//           <a href="">
+//             <i className="ri-phone-fill "></i>
+//           </a>
+//         </div>
+//         <div className="w-[1px] h-32 bg-[#915EFF] sm:hidden"></div>
+//       </div>
+//     </div>
+//   );
+// };
 
-export default leftSider;
+// export default leftSider;
